c07c88c4ba431f777794c28507eb8859896f6e57
lindong <lindong@192.168.3.4>
2020-06-10 17:46:49 +0900

    Jetty server started

--- src/main/java/org/scot/FixWuiEx.java
+++ src/main/java/org/scot/FixWuiEx.java
@@ -1,23 +1,53 @@
 package org.scot;
 
-import java.io.IOException;
+import java.net.URI;
+
+import javax.ws.rs.core.UriBuilder;
+
+import org.eclipse.jetty.server.Server;
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
+import org.glassfish.jersey.jetty.JettyHttpContainerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
 
 public class FixWuiEx {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	FIXService fixService;
+	Server jetty;
 
-		FIXService fixService = new FIXServiceImpl();
-		fixService.init();
-		
-		System.out.println("press <enter> to quit");
+	public static void main(String[] args) {
+			FixWuiEx fixWuiEx = new FixWuiEx();
+			fixWuiEx.start();
+	}
+
+	private void start() {
+		// TODO Auto-generated method stub
 		try {
+
+			fixService = new FIXServiceImpl();
+			fixService.init();
+
+			URI baseUri = UriBuilder.fromUri("http://localhost/").port(9998).build();
+			ResourceConfig config = new ResourceConfig();
+			config.packages("org.scot");
+			config.register(new AbstractBinder(){
+
+				@Override
+				protected void configure() {
+					bind(fixService).to(FIXService.class);
+				}
+			});
+			
+			
+			jetty = JettyHttpContainerFactory.createServer(baseUri, config);
+			jetty.start();
+
+			System.out.println("press <enter> to quit");
 			System.in.read();
-		} catch (IOException e) {
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
+		
 	}
 
 }
